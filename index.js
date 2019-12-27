78777ea3cf7f8684c42e4e1ec3f296ad5de727f6
Ria <riadipa@gmail.com>
2019-12-27 22:48:30 +0100

    database is added to heroku

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const cors = require("cors");
+const Person = require("./models/person");
 
 app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use(express.static("build"));
 
 morgan.token("body", function(req, res) {
   return JSON.stringify(req.body);
@@ -13,10 +19,6 @@ morgan.token("body", function(req, res) {
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
-
-app.use(cors());
-
-app.use(express.static("build"));
 
 let persons = [
   {
@@ -42,44 +44,59 @@ let persons = [
 ];
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then(persons => {
+    response.json(persons.map(person => person.toJSON()));
+  });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find(person => person.id === id);
-  console.log("person", person);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person.toJSON());
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(error => next(error));
 });
 
 app.get("/info", (request, response) => {
   var dateTime = new Date();
-  const totalId = Math.max(...persons.map(p => p.id));
-  console.log("totalId", totalId);
-  return response.send(
-    `Phonebook has info for ${totalId} people` + "<br/>" + dateTime
-  );
+  Person.countDocuments().then(number => {
+    response.send(
+      `Phonebook has info for ${number} people` + " <br/> " + dateTime
+    );
+  });
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter(person => person.id !== id);
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+  console.log("body", body);
 
-  response.status(204).end();
+  const person = {
+    name: body.name,
+    number: body.number
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson.toJSON());
+    })
+    .catch(error => next(error));
 });
 
-const generateId = () => {
-  return Math.floor(Math.random() * 10000);
-};
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end();
+    })
+    .catch(error => next(error));
+});
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
   console.log("body name: ", body.name);
-  console.log("person name: ", persons);
 
   if (!body.name) {
     return response.status(400).json({
@@ -93,27 +110,27 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  const existingPerson = persons.filter(
-    person => person.name.toUpperCase() === body.name.toUpperCase()
-  );
-  console.log("existing Persons name: ", existingPerson);
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  });
 
-  if (existingPerson.length === 1) {
-    return response.status(400).json({
-      error: "name must be unique"
-    });
+  person.save().then(savedPerson => {
+    response.json(savedPerson.toJSON());
+  });
+});
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError" && error.kind == "ObjectId") {
+    return response.status(400).send({ error: "malformatted id" });
   }
 
-  const personObj = {
-    name: body.name,
-    number: body.number,
-    id: generateId()
-  };
+  next(error);
+};
 
-  persons = persons.concat(personObj);
-
-  response.json(personObj);
-});
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 
